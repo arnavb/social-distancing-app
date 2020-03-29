66493aa02d831d94ee47859c4805971aae956a3e
Sagar Patil <sagarreddypatil@gmail.com>
2020-03-29 15:23:48 -0400

    use react template

--- frontend-mobile/App.js
+++ frontend-mobile/App.js
@@ -1,19 +1,79 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React from "react";
+import { Image } from "react-native";
+import { AppLoading } from "expo";
+import { Asset } from "expo-asset";
+import { Block, GalioProvider } from "galio-framework";
+import { NavigationContainer } from "@react-navigation/native";
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-    </View>
-  );
+// Before rendering any navigation stack
+import { enableScreens } from "react-native-screens";
+enableScreens();
+
+import Screens from "./navigation/Screens";
+import { Images, articles, argonTheme } from "./constants";
+
+// cache app images
+const assetImages = [
+  Images.Onboarding,
+  Images.LogoOnboarding,
+  Images.Logo,
+  Images.Pro,
+  Images.ArgonLogo,
+  Images.iOSLogo,
+  Images.androidLogo
+];
+
+// cache product images
+articles.map(article => assetImages.push(article.image));
+
+function cacheImages(images) {
+  return images.map(image => {
+    if (typeof image === "string") {
+      return Image.prefetch(image);
+    } else {
+      return Asset.fromModule(image).downloadAsync();
+    }
+  });
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+export default class App extends React.Component {
+  state = {
+    isLoadingComplete: false
+  };
+
+  render() {
+    if (!this.state.isLoadingComplete) {
+      return (
+        <AppLoading
+          startAsync={this._loadResourcesAsync}
+          onError={this._handleLoadingError}
+          onFinish={this._handleFinishLoading}
+        />
+      );
+    } else {
+      return (
+        <NavigationContainer>
+          <GalioProvider theme={argonTheme}>
+            <Block flex>
+              <Screens />
+            </Block>
+          </GalioProvider>
+        </NavigationContainer>
+      );
+    }
+  }
+
+  _loadResourcesAsync = async () => {
+    return Promise.all([...cacheImages(assetImages)]);
+  };
+
+  _handleLoadingError = error => {
+    // In this case, you might want to report the error to your error
+    // reporting service, for example Sentry
+    console.warn(error);
+  };
+
+  _handleFinishLoading = () => {
+    this.setState({ isLoadingComplete: true });
+  };
+}
---
+++ frontend-mobile/CHANGELOG.md
@@ -0,0 +1,61 @@
+## [1.4.0] 2020 - 03 - 05
+### Removed dependencies
+- removed `react-navigation@3.11.0`
+
+### Added dependencies
+- added `@react-navigation/compat@5.0.0`
+- added `@react-navigation/drawer@5.0.0`
+- added `@react-navigation/native@5.0.0`
+- added `@react-navigation/stack@5.0.0`
+- added `@react-native-community/masked-view@0.1.5`
+- added `react-native-reanimated@1.4.0`
+- added `react-native-safe-area-context@0.6.0`
+- added `react-native-screeens@2.0.0-alpha.12`
+
+### Updated dependencies
+- updated `expo@35.0.0` to `expo@36.0.0`
+- updated `expo-asset@7.0.0` to `expo-asset@8.0.0`
+- updated `expo-font@7.0.0` to `expo-font@8.0.0`
+- updated `expo-cli@2.4.0` to `expo-cli@3.11.7`
+- updated `expo-linear-gradient@7.0.0` to `expo-linear-gradient@8.0.0`
+- updated `react@16.8.3` to `react@16.9.0`
+
+### Updated files
+- changed the whole routing from `Screens.js` because `react-navigation@5.0.0` has a new dynamic API
+- changed `Menu.js` for a new Drawer custom component
+- changed `DrawerItem.js` for a new type of `<DrawerCustomItem />`
+- changed props and variables so that the new `react-navigation` API could work with the following files: `Beauty.js`, `Header.js`, `Product.js`, `Gallery.js`, `Pro.js`, `Product.js`, `Settings.js`, `Register.js`, `Onboarding.js`
+
+## [1.3.0] 2019-11-06
+### Updated dependencies
+- `expo@34.0.3` to `expo@35.0.0`
+- `expo-font@6.0.1` to `expo-font@7.0.0`
+- `expo-asset@6.0.0` to `expo-asset@7.0.0`
+- `react-native SDK@34.0.0` to `react-native SDK@35.0.0`
+- `galio-framework@0.6.1` to `galio-framework@0.6.3`
+- `babel-preset-expo@5.0.0` to `babel-preset-expo@7.0.0`
+
+### Updated files
+- changed drawer items in order to have a similar look to the PRO version
+
+## [1.2.0] 2019-09-18
+### Updated dependencies
+- `expo@33.0.0` to `expo@34.0.3`
+- `expo-font@5.0.1` to `expo-font@6.0.1`
+- added `expo-asset@6.0.0`
+- `react-native SDK@33.0.0` to `react-native SDK@34.0.0`
+- added `react-native-gesture-handler@1.3.0`
+- `galio-framework@0.5.3` to `galio-framework@0.6.1`
+
+## [1.1.0] 2019-06-21
+### Updated dependencies
+- `expo@32.0.0` to `expo@33.0.0`
+- `galio-framework@0.5.1` to `galio-framework@0.5.3`
+- `react-native SDK@32.0.0` to `react-native SDK@33.0.0`
+- `react-navigation@3.8.1` to `react-navigation@3.11.0`
+- `react@16.5.0` to `react@16.8.3`
+- added `expo-font@5.0.1`
+
+
+## [1.0.0] 2019-06-07
+### Initial Release
