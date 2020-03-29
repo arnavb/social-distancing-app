f3cf95e7520d8b2ebd5a42e120ee52dd28cda0c9
Arnav Borborah <arnavborborah11@gmail.com>
2020-03-29 13:54:57 -0400

    Setup live popularity for a test location

--- backend/api.py
+++ backend/api.py
@@ -11,11 +11,18 @@ app = Flask(__name__)
 
 
 @app.route("/")
-def index():
-    result = populartimes.get_id(
-        os.environ["GMAPS_PLACES_API_KEY"], "ChIJIz36HAG3w4kRvB--Pl2EjIw"
+def get_current_popularity():
+    """Get the current popularity, if it exists, for the location passed
+    as a parameter"""
+
+    json_data = populartimes.get_id(
+        os.environ["GMAPS_PLACES_API_KEY"], "ChIJc_bAtSDIw4kRnfrNZxYHK_M"
     )
-    return jsonify(result)
+    if "current_popularity" in json_data:
+        current_popularity = json_data["current_popularity"]
+        return jsonify(current_popularity=current_popularity)
+
+    return jsonify(error="NO_LIVE_DATA")
 
 
 if __name__ == "__main__":
