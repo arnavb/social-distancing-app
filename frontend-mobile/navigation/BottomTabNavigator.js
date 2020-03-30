25d671c654bddceb3172f0d3369cd49c7a035fe1
Sagar Patil <sagarreddypatil@gmail.com>
2020-03-29 22:36:25 -0400

    make location work

---
+++ frontend-mobile/navigation/BottomTabNavigator.js
@@ -0,0 +1,48 @@
+import * as React from 'react';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import TabBarIcon from '../components/TabBarIcon';
+import HomeScreen from '../screens/HomeScreen';
+import LinksScreen from '../screens/LinksScreen';
+import AlertScreen from '../screens/AlertScreen';
+
+const BottomTab = createBottomTabNavigator();
+const INITIAL_ROUTE_NAME = 'Home';
+
+export default function BottomTabNavigator({ navigation, route }) {
+  // Set the header title on the parent stack navigator depending on the
+  // currently active tab. Learn more in the documentation:
+  // https://reactnavigation.org/docs/en/screen-options-resolution.html
+  navigation.setOptions({ headerTitle: getHeaderTitle(route) });
+
+  return (
+    <BottomTab.Navigator initialRouteName={INITIAL_ROUTE_NAME}>
+      <BottomTab.Screen
+        name="Alert"
+        component={AlertScreen}
+        options={{
+          title: 'Alert',
+          tabBarIcon: ({ focused }) => <TabBarIcon focused={focused} name="md-alert" />,
+        }}
+      />
+      <BottomTab.Screen
+        name="Home"
+        component={HomeScreen}
+        options={{
+          title: 'Home',
+          tabBarIcon: ({ focused }) => <TabBarIcon focused={focused} name="md-book" />,
+        }}
+      />
+    </BottomTab.Navigator>
+  );
+}
+
+function getHeaderTitle(route) {
+  const routeName = route.state?.routes[route.state.index]?.name ?? INITIAL_ROUTE_NAME;
+
+  switch (routeName) {
+    case 'Alert':
+      return 'Configure Crowd Alerts';
+    case 'Links':
+      return 'Links to learn more';
+  }
+}
