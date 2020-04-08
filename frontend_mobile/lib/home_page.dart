386e739384faaf85ec950e6ab9e81da578bea581
Arnav Borborah <arnavborborah11@gmail.com>
2020-04-07 21:49:29 -0400

    Finish setting up live location info

--- frontend_mobile/lib/home_page.dart
+++ frontend_mobile/lib/home_page.dart
@@ -1,4 +1,7 @@
+import 'dart:async';
+
 import 'package:flutter/material.dart';
+import 'package:geolocation/geolocation.dart';
 
 class HomePage extends StatefulWidget {
   @override
@@ -6,6 +9,29 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
+  StreamSubscription<LocationResult> _locationSubscription;
+  String _currentLocation = "Unknown";
+
+  @override
+  void dispose() {
+    super.dispose();
+    _locationSubscription.cancel();
+  }
+
+  @override
+  void initState() {
+    super.initState();
+
+    _locationSubscription = Geolocation.locationUpdates(
+            accuracy: LocationAccuracy.best, inBackground: true)
+        .listen((result) {
+      setState(() {
+        _currentLocation =
+            "${result.location.latitude}, ${result.location.latitude}";
+      });
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -13,26 +39,8 @@ class _HomePageState extends State<HomePage> {
         title: Text("Home"),
       ),
       body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            if (true)
-              Padding(
-                padding: const EdgeInsets.all(8.0),
-                child: Text("SHM"),
-              ),
-            RaisedButton(
-              child: Text("Get User Location"),
-              onPressed: _getCurrentLocation
-            )
-          ],
-        ),
-      )
+        child: Text(_currentLocation),
+      ),
     );
   }
-
-  void _getCurrentLocation() {
-  }
 }
-
-
