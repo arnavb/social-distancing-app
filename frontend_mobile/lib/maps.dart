9311d87b116a197a4f1272b9bd69b4e1d472c5b7
Arnav Borborah <arnavborborah11@gmail.com>
2020-04-09 14:35:57 -0400

    Add custom styling to embedded map

--- frontend_mobile/lib/maps.dart
+++ frontend_mobile/lib/maps.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
+import 'package:flutter/services.dart';
 
 class Map extends StatefulWidget {
   @override
@@ -7,13 +8,23 @@ class Map extends StatefulWidget {
 }
 
 class _MapState extends State<Map> {
+  String _mapStyle;
   GoogleMapController mapController;
 
   final LatLng _center =
       const LatLng(37.4040, -122.1075); //<=========PUT LATITUDE, LONGITUDE HERE
 
+  @override
+  void initState() {
+    super.initState();
+    rootBundle.loadString('assets/map_style.json').then((result) {
+      _mapStyle = result;
+    });
+  }
+
   void _onMapCreated(GoogleMapController controller) {
     mapController = controller;
+    mapController.setMapStyle(_mapStyle);
   }
 
   @override
