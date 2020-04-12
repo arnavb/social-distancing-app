4c3c89ba45ef3d4fb8f5d39a6045f63ce55b3b37
Rithvik Kasarla <rithvikrk719@gmail.com>
2020-04-11 22:12:20 -0400

    Started home page (images are placeholders)

---
+++ frontend_mobile/lib/home.dart
@@ -0,0 +1,33 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+void main() => runApp(buildScreen());
+
+class buildScreen extends StatelessWidget{
+  @override
+  Widget build(BuildContext context){
+    return MaterialApp(
+      home: Stack(
+          children: <Widget>[
+            Container(
+              color:Colors.white,
+            ),
+            Container(
+              decoration: BoxDecoration(
+                image:DecorationImage(
+                    image: NetworkImage('https://vignette.wikia.nocookie.net/memepediadankmemes/images/b/be/Spaghet.png/revision/latest/top-crop/width/360/height/450?cb=20180611153100'),
+                    fit: BoxFit.cover
+                  )
+                )
+            ),
+            Container(
+              alignment: Alignment.topCenter,
+              width: 500,
+              height:500,
+              child: Image.network('https://upload.wikimedia.org/wikipedia/commons/thumb/8/82/SARS-CoV-2_without_background.png/597px-SARS-CoV-2_without_background.png'),
+            )
+          ]
+      ),
+    );
+  }
+}
