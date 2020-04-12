3ea2ce846bfcdaf14a605a4d80781846cc703177
Sagar Patil <sagarreddypatil@gmail.com>
2020-04-12 19:26:41 -0400

    Change app title

--- frontend_mobile/lib/main.dart
+++ frontend_mobile/lib/main.dart
@@ -14,7 +14,7 @@ class App extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-        title: 'Flutter Demo',
+        title: 'Social Distancing App',
         theme: ThemeData(primaryColor: Colors.white),
         darkTheme: ThemeData.dark(),
         home: Map());
