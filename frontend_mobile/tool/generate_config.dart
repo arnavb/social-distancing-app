60809fa29251554729a7beaa9f9011f56495d004
Arnav Borborah <arnavborborah11@gmail.com>
2020-04-12 21:14:44 -0400

    Fix incorrect key name

--- frontend_mobile/tool/generate_config.dart
+++ frontend_mobile/tool/generate_config.dart
@@ -4,9 +4,9 @@ import 'dart:io';
 Future<void> main() async {
   final config = {
     'apiUrl': Platform.environment['API_URL'],
-    'googleApiUrl': Platform.environment['GOOGLE_API_KEY']
+    'googleApiKey': Platform.environment['GOOGLE_API_KEY']
   };
-  
+
   final filename = 'lib/.env.dart';
   File(filename).writeAsString('final environment = ${json.encode(config)};');
 }
