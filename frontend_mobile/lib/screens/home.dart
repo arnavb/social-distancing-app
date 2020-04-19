db46a0acab1ab43f2428cfae0723ff89d033d6db
Arnav Borborah <arnavborborah11@gmail.com>
2020-04-19 12:26:40 -0400

    Reformat files with dartfmt

--- frontend_mobile/lib/screens/home.dart
+++ frontend_mobile/lib/screens/home.dart
@@ -13,55 +13,47 @@ class _BuildScreenState extends State<BuildScreen> {
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
-        body: Stack(
-            fit: StackFit.expand,
+        body: Stack(fit: StackFit.expand, children: <Widget>[
+          Container(
+            color: Colors.white,
+          ),
+          Container(
+              decoration: BoxDecoration(
+                  image: DecorationImage(
+                      image: AssetImage(
+                          'assets/images/GrayScaleSpaceBackround.jpg'),
+                      fit: BoxFit.cover))),
+          Positioned(
+              right: 0,
+              left: 3,
+              top: 190,
+              child: Container(
+                width: 350,
+                height: 350,
+                child: Image.asset('assets/images/CaronaPicture.png'),
+              )),
+          Column(
+            mainAxisAlignment: MainAxisAlignment.center,
             children: <Widget>[
-              Container(
-                color: Colors.white,
-              ),
-              Container(
-                  decoration: BoxDecoration(
-                      image: DecorationImage(
-                          image: AssetImage(
-                              'assets/images/GrayScaleSpaceBackround.jpg'),
-                          fit: BoxFit.cover)
-                  )
-              ),
-              Positioned(
-                  right: 0,
-                  left: 3,
-                  top: 190,
-                  child: Container(
-                    width: 350,
-                    height: 350,
-                    child: Image.asset('assets/images/CaronaPicture.png'),
-                  )
-              ),
-              Column(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: <Widget>[
-                  Expanded(
-                    flex: 1,
-                    child: Align(
-                        alignment: Alignment.bottomCenter,
-                        child: Column(
-                          mainAxisAlignment: MainAxisAlignment.center,
-                          children: <Widget>[
-                            CircularProgressIndicator(),
-                            Padding(
-                              padding: EdgeInsets.only(top: 10),
-                            ),
-                            Text(
-                              "Stay Safe \n and \n Social Distance"
-                            )
-                          ],
+              Expanded(
+                  flex: 1,
+                  child: Align(
+                    alignment: Alignment.bottomCenter,
+                    child: Column(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: <Widget>[
+                        CircularProgressIndicator(),
+                        Padding(
+                          padding: EdgeInsets.only(top: 10),
                         ),
-                        )
-                      )
+                        Text("Stay Safe \n and \n Social Distance")
                       ],
                     ),
-                  ]),
-              ),
+                  ))
+            ],
+          ),
+        ]),
+      ),
     );
   }
 }
