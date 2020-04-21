1689d9f1ec8c141c78dad27430d9f315f42d54fc
Arnav Borborah <arnavborborah11@gmail.com>
2020-04-20 21:56:38 -0400

    Finish setting up loading screen

--- frontend_mobile/lib/screens/home.dart
+++ frontend_mobile/lib/screens/home.dart
@@ -3,55 +3,46 @@ import 'package:flutter/material.dart';
 
 void main() => runApp(BuildScreen());
 
-class BuildScreen extends StatefulWidget {
-  @override
-  _BuildScreenState createState() => new _BuildScreenState();
-}
-
-class _BuildScreenState extends State<BuildScreen> {
+class BuildScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
         body: Stack(fit: StackFit.expand, children: <Widget>[
           Container(
-            color: Colors.white,
-          ),
-          Container(
               decoration: BoxDecoration(
+                  color: Colors.black,
                   image: DecorationImage(
                       image: AssetImage(
                           'assets/images/GrayScaleSpaceBackround.jpg'),
-                      fit: BoxFit.cover))),
-          Positioned(
-              right: 0,
-              left: 3,
-              top: 190,
-              child: Container(
+                      fit: BoxFit.cover,
+                      colorFilter: ColorFilter.mode(
+                          Colors.black.withOpacity(0.4), BlendMode.dstATop)))),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              Container(
                 width: 350,
                 height: 350,
                 child: Image.asset('assets/images/CaronaPicture.png'),
-              )),
-          Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: <Widget>[
-              Expanded(
-                  flex: 1,
-                  child: Align(
-                    alignment: Alignment.bottomCenter,
-                    child: Column(
-                      mainAxisAlignment: MainAxisAlignment.center,
-                      children: <Widget>[
-                        CircularProgressIndicator(),
-                        Padding(
-                          padding: EdgeInsets.only(top: 10),
-                        ),
-                        Text("Stay Safe \n and \n Social Distance")
-                      ],
-                    ),
-                  ))
+              )
             ],
           ),
+          Positioned(
+            bottom: 100,
+            left: 0,
+            right: 0,
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: <Widget>[
+                CircularProgressIndicator(),
+                Text(
+                  "\nStay safe",
+                  style: TextStyle(color: Colors.white, fontSize: 30),
+                )
+              ],
+            ),
+          )
         ]),
       ),
     );
